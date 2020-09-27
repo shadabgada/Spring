b7842228bba086780a6298b23abea389bc57f9c9
Shadab Gada <shadabgada234@gmail.com>
2020-09-27 23:36:20 +0530

    added pointcuts

--- Spring-AOP/src/main/java/com/example/SpringAOP/aspect/CommonJoinPointConfig.java
+++ Spring-AOP/src/main/java/com/example/SpringAOP/aspect/CommonJoinPointConfig.java
@@ -5,4 +5,21 @@ import org.aspectj.lang.annotation.Pointcut;
 public class CommonJoinPointConfig {
     @Pointcut("execution(* com.example.SpringAOP.business.*.*(..))")
     public void BusinessLayerExecution(){}
+
+    @Pointcut("execution(* com.example.SpringAOP.data.*.*(..))")
+    public void DataLayerExecution(){}
+
+    @Pointcut(" com.example.SpringAOP.aspect.CommonJoinPointConfig.DataLayerExecution() && com.example.SpringAOP.aspect.CommonJoinPointConfig.BusinessLayerExecution()")
+    public void AllLayer(){}
+
+    //All beans containing dao
+    @Pointcut("bean(*dao*)")
+    public void beanContainingDAO(){}
+
+    @Pointcut("bean(dao*)")
+    public void beanStartingWithDAO(){}
+
+    //Within Data Layer
+    @Pointcut("within(com.example.SpringAOP.data..*)")
+    public void withinDataLayer(){}
 }
